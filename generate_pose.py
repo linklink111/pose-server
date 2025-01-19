956f7430ecfc3e5bd1ac2d9c145f5597af46b56b
linlink0000 <>
2025-01-19 13:00:42 +0800

    Update generate_pose.py

--- generate_pose.py
+++ generate_pose.py
@@ -4,7 +4,7 @@ import time
 import os
 import re
 
-co = cohere.ClientV2("")
+co = cohere.ClientV2("HTwK0MTkqJuhqY3PZEUoBlNAmWPSSEOQMFWlVR0J")
 
 def query_command_r_plus_08_2024(user_prompt, system_prompt=None):
     if system_prompt is None:
@@ -47,16 +47,30 @@ def query_command_r_plus_08_2024_streaming(user_prompt, system_prompt=None):
         return None
 
 # Theoritical Teacher in the overview graph
+
 def planning_pose(user_prompt):
-    system_prompt = '''You are a helpful assistant. You can plan how to make a motion animation for a character.
-Given the description of thr motion, you can smartly choose a scheme to make it.
-You can choose a scheme from below:
+    system_prompt = '''You are a helpful assistant. Your task is to plan a motion animation for a character based on the given description.
+You can choose the most suitable approach from the following options:
 
-1. Decomposite to Key Poses. This scheme is good for substantial motion.
+1. Decompose into Key Poses: This approach is ideal for complex motions.
 
-2. Base pose and partial movement. This scheme is good for simple motion.
+2. Base Pose with Partial Movements: This approach is suitable for simpler motions.
+
+You should select the appropriate approach and provide the reason for your choice in the following format:
+{
+    "scheme": "Decompose into Key Poses",
+    "reason": "The motion is complex, with significant differences between each key pose. Therefore, it should be decomposed into key poses.",
+    "choice": 1
+}
+
+Another example:
+{
+    "scheme": "Base Pose with Partial Movements",
+    "reason": "The motion is repetitive, with the overall body remaining relatively still while only the hand moves back and forth. Therefore, it is better to use the base pose with partial movements approach.",
+    "choice": 2
+}
 '''
-# Pose Designer + Physical Teacher(Relative Position)
+    return query_command_r_plus_08_2024(user_prompt, system_prompt)# Pose Designer + Physical Teacher(Relative Position)
 def generate_pose_design(user_prompt):
     system_prompt = '''You are a helpful assistant. You can design a pose for a character.
     You should output it as a JSON object, with the following keys:
